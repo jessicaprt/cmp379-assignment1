352b38f48ce6a5de5a9551139501b1d88241e6d8
jessicaprt <jessicaprieto31@gmail.com>
2016-09-23 00:03:29 -0600

    initialize repo

---
+++ memlayout.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <setjmp.h>
+#include <stdint.h>
+#include "memlayout.h"
+
+typedef int bool;
+#define true 1
+#define false 0
+
+sigjmp_buf env;
+
+void sigsev_handler(int sig) {
+	siglongjmp(env, 2);
+}
+
+int get_mem_layout (struct memregion * regions, unsigned int size) {
+	int fd = open("/dev/zero", O_RDONLY);
+	char sample;
+	int curr_size = 0;
+	int curr_mode = 0;
+	void * prev = 0;
+	void * curr = 0;
+
+	struct sigaction act;
+	struct sigaction prev_act;
+
+	act.sa_handler = sigsev_handler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+
+	sigaction(SIGSEV, &act, &prev_act);
+
+	regions[0].from=curr;
+
+	bool check = true;
+
+	if(sigsetjmp(env,1) == 2) {
+		regions[0].mode = MEM_NO;
+		curr_mode = MEM_NO;
+		check = false;
+	}
+
+	if (check){
+		sample = *curr;
+		
+		if (read(fd, curr, 1) == 1) {
+			regions[0].mode = MEM_RW;
+			curr_mode = MEM_RW;
+			*curr = sample;
+		} else {
+			regions[0].mode = MEM_RO;
+			curr_mode = MEM_RO;
+		}
+	}
+
+	while((uint32_t) curr >= (uint32_t) prev) {
+		check = true;
+		if(sigsetjmp(env,1) == 2) {
+			if (curr_mode != MEM_NO) {
+				curr_size = curr_size + 1;
+				curr_mode = MEM_NO;
+				
+				if (curr_size - 1 < size) {
+					regions[curr_size].to = curr-1;
+
+					if(curr_size < size) {
+						regions[curr_size].to = curr;
+						regions[curr_size].mode = MEM_NO;
+					}
+
+				}
+			}
+			check = false;
+		}
+
+		if(check){
+			check = false
+			sample = *curr;
+
+			if (read(fd, curr, 1) == 1) {
+				// can read.. MEM_RW
+				*curr = sample;
+
+
+			} else {
+				// MEM_RO
+			}
+		}
+		prev = curr;
+		curr = curr + PAGE_SIZE;
+	}
+	close(fd);
+
+	sigaction(SIGSEV, &prev_act, 0);
+
+	return curr_size;
+
+}
+
+int get_mem_diff (struct memregion * regions, unsigned int howmany,
+					struct memregion * thediff, unsigned int diffsize) {
+}
---
+++ memlayout.h
@@ -0,0 +1,13 @@
+#define MEM_RW 0;
+#define MEM_RO 1;
+#define MEM_NO 2;
+
+struct memregion {
+	void * from;
+	void * to;
+	unsigned char mode;
+}
+
+int get_mem_layout (struct memregion * regions, unsigned int size);
+int get_mem_diff (struct memregion * regions, unsigned int howmany,
+					struct memregion * thediff, unsigned int diffsize);
