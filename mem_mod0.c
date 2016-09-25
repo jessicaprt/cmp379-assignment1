ff9009f113c2dfb98b37395670cfcb7ced710533
Dannick <dannick@e5-12-25.ece.ualberta.ca>
2016-09-24 18:22:41 -0600

    Array size, good bye cruel world.

--- mem_mod0.c
+++ mem_mod0.c
@@ -2,15 +2,15 @@
 
 #include "memlayout.h"
 
-#define REGIONS_SIZE 4
+#define REGIONS_SIZE 32
 
 int PAGE_SIZE = 4096;
 
 int main(int argc, char** argv){
 	struct memregion regions[REGIONS_SIZE];
 
-	get_mem_layout(regions, REGIONS_SIZE);
+	int numregions = get_mem_layout(regions, REGIONS_SIZE);
+	print_regions(regions, numregions, REGIONS_SIZE); 
 
-	printf("Hello world\n");
 	return 0;
 }
