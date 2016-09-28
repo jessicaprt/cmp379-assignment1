8e6447cfaea8db822c000b1d89762f23a299748f
jessicaprt <jessicaprieto31@gmail.com>
2016-09-27 18:53:48 -0600

    mem_mod1

---
+++ mem_mod1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include "memlayout.h"
+#define REGIONS_SIZE 32
+#define DIFF_SIZE 32
+
+int PAGE_SIZE = 4096;
+
+int main(int argc, char** argv){
+	struct memregion regions[REGIONS_SIZE];
+	struct memregion thediff[DIFF_SIZE];
+
+	//call get_mem_layout
+	int numregions = get_mem_layout(regions, REGIONS_SIZE);
+	print_regions(regions, numregions, REGIONS_SIZE); 
+
+	//void *malloc(PAGE_SIZE);
+	mmap(NULL, PAGE_SIZE, PROT_READ, MAP_ANONYMOUS, 0, 0);
+
+	//call get_mem_diff
+	int diffregions = get_mem_diff(regions, diffregions, thediff, DIFF_SIZE);
+	print_regions(thediff, diffregions, DIFF_SIZE);
+
+	return 0;
+}
